aef17ab36bf852a5c382379230c9831584e8a5fe
Lindemberg <lindembergncastro@outlook.com>
2020-03-29 13:09:34 -0300

    frontend ready

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -4,7 +4,7 @@ module.exports = {
     const { page = 1 } = request.query;
     const [count] = await connection("incidents").count();
     const incidents = await connection("incidents")
-      .join("ongs", "ongs.id", "=", "incidents.ongs_id")
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
       .limit(5)
       .offset((page - 1) * 5)
       .select([
